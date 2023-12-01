0fa043ba281ba9a648ace2044cf8692642e79ef5
sundasalmani18 <sundasalmani18@gmail.com>
2023-12-01 17:24:18 +0500

    update

---
+++ javascriptlearning/assets/js/app.js
@@ -0,0 +1,72 @@
+// document.getElementById("hello").innerHTML = "Hello Javacsript";
+// document.getElementById("addition").innerHTML = 10 + 100;
+// window.alert("I'am working")
+// alert("Hello world");
+
+// document.write(40 + 60);
+
+let name = "ali";
+console.log("My name is: ", name);
+name = "jhon";
+console.info("My name is: " + name);
+name = "aleena";
+console.log(`My name is: ${name}`);
+
+
+let num = 123
+console.error('Number is ${num}');
+
+
+// let firstName = "ali" , lastName = "waqas" , gender = "male";
+let firstName = "ali";
+let lastName = "waqas";
+let gender = "male";
+
+let numberOne = 50.5;
+let numberTwo = 100;
+let result = numberOne + numberTwo;
+
+document.write(`My name ${firstName} ${lastName}. my gender ${gender}. total masks ${result}`);
+console.info("My name is: " + name + " my gender " + gender);
+
+
+//  this function display text
+
+/*
+lajsdf
+faldfa
+kfahksdhf
+sadfasdf
+*/
+
+var country = "pakistan"
+var country = "India"
+
+let city = "karachi"
+// let city = "karachi"
+
+const countries = ["karachi", "Lahore"];
+console.log(countries[1])
+countries[1] = "Sindh"
+console.log(countries[1])
+countries.push("Lahore")
+console.log(countries)
+
+const car = { modal: "2002", color: "red", brand: "toyota" }
+console.log(car.color)
+car.color = "white"
+console.log(car.color)
+car.door = "two"
+console.log(car)
+
+function myfunc() {
+    // let city = "lahore"
+    // console.log('abc', country)
+    // console.log('', city)
+    console.log('', town)
+    document.getElementById("hello").innerHTML = "Hello Javacsript";
+    document.getElementById("addition").innerHTML = 10 + 100;
+}
+
+
+// Variable
