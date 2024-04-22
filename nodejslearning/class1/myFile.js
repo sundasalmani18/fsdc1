b1ea81751db95c04aa4322e78d6e41abff93999c
sundasalmani18 <sundasalmani18@gmail.com>
2024-04-22 20:20:59 +0500

    update

--- nodejslearning/class1/myFile.js
+++ nodejslearning/class1/myFile.js
@@ -8,15 +8,43 @@ http.createServer(function (req,res){
         return res.end()
     })
 
-    fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
+    // fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    //     if (err) throw err;
+    //     console.log('Saved!');
+    //   });
 
-      fs.open('mynewfile2.txt', 'w', function (err, file) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
+    //   fs.open('mynewfile2.txt', 'w', function (err, file) {
+    //     if (err) throw err;
+    //     console.log('Saved!');
+    //   });
+
+    
+// fs.writeFile('mynewfile2.txt', 'Hello content!', function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+// fs.appendFile('mynewfile2.txt', ' This is my text.', function (err) {
+//   if (err) throw err;
+//   console.log('Updated!');
+// });
+
+// fs.writeFile('mynewfile2.txt', 'This is my Replaced text', function (err) {
+//   if (err) throw err;
+//   console.log('Replaced!');
+// });
+
+
+// fs.rename('mynewfile2.txt', 'myrenamedfile.txt', function (err) {
+//   if (err) throw err;
+//   console.log('File Renamed!');
+// });
+
+fs.unlink('mynewfile1.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});
+
 }).listen(8000)
 
 
