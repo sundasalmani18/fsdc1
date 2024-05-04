52f0608ccb214f4d2a3d78e6d35d9364f3de864a
sundasalmani18 <sundasalmani18@gmail.com>
2024-05-04 17:38:37 +0500

    update

--- nodejslearning/class2/uploadfilemulter.js
+++ nodejslearning/class2/uploadfilemulter.js
@@ -3,7 +3,18 @@ const path = require('path');
 const multer =require('multer');
 
 const router = express.Router();
-const upload = multer({dest:"/uploads"})
+// const upload = multer({dest:"/uploads"})
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/'); // Destination folder for uploaded files
+    },
+    filename: (req, file, cb) => {
+      cb(null,Date.now() + '-' + file.originalname); // Rename the file to include the timestamp
+    },
+  });
+  const upload = multer({ storage: storage });
 
 router.get("/",(req,res)=>{
     return res.render("uploadview")
