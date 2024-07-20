f57affedf4c5b05fb81111fd426c108b38202e68
sundasalmani18 <sundasalmani18@gmail.com>
2024-07-20 18:16:36 +0500

    update

--- nodejslearning/project/Ecommerce/index.js
+++ nodejslearning/project/Ecommerce/index.js
@@ -1,12 +1,16 @@
-
-
 import express from 'express';
+import authRoutes from './routes/authRoutes';
+
+
+
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use("/auth",authRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
