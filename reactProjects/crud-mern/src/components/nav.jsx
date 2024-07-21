87de399b86e007035fe20a66ba81642ee4686bac
sundasalmani18 <sundasalmani18@gmail.com>
2024-07-21 17:31:15 +0500

    update

--- reactProjects/crud-mern/src/components/nav.jsx
+++ reactProjects/crud-mern/src/components/nav.jsx
@@ -1,17 +1,19 @@
-
+import { useNavigate } from "react-router-dom";
 
 export default  function Nav(){
+  const navigate =useNavigate();
     return(
    <>
   <ul className="nav justify-content-end">
   <li className="nav-item">
-    <a className="nav-link active" href="#">Home</a>
+    <a className="nav-link active" href="/">Home</a>
   </li>
   <li className="nav-item">
     <a className="nav-link" href="#">Category</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#">Register</a>
+  <a id="headbutton" onClick={()=>navigate("/register")}  className="btn-sm btn btn-outline-success ">Register</a>
+    
   </li>
   <li className="nav-item">
     <a className="nav-link disabled" href="#">Login</a>
--- reactProjects/crud-mern/src/header-nav/LayoutRoutes.jsx
+++ reactProjects/crud-mern/src/header-nav/LayoutRoutes.jsx
@@ -1,6 +1,8 @@
 import { Routes,Route } from "react-router-dom";
 import ContactusPages from "../pages/contactuspages";
 import { HomePage } from "../pages/Homepages";
+import Register from "../components/register";
+import Login from "../components/login";
 
 
 
@@ -8,7 +10,8 @@ export function Layout (){
  return(
   <>
   <Routes>
-
+  <Route path="/login" element={<Login/>}/>
+  <Route path="/register" element={<Register/>}/>
     <Route path="/contactus" element={<ContactusPages/>}/>
     <Route path="/" element={<HomePage/>}/>
 
