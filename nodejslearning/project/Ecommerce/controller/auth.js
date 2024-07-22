ae964f6103ebbab99b8d4a07a1eead4e5c577fd4
sundasalmani18 <sundasalmani18@gmail.com>
2024-07-22 16:55:26 +0500

    update

--- nodejslearning/project/Ecommerce/controller/auth.js
+++ nodejslearning/project/Ecommerce/controller/auth.js
@@ -1,3 +1,4 @@
+import nodemon from "nodemon";
 import { db } from "../dbCon";
 
 export const register=(req ,res)=>{
@@ -33,10 +34,46 @@ return res.status(200).json({Data:responseData})
 
     })
 
-
-
-
   })
 
 }
-   
+
+export const login=(req ,res)=>{
+const email = req.body.email
+const password = req.body.password
+
+const query ="SELECT * FROM user where email =? "
+
+db.query(query,[email],(err,data)=>{
+
+  if (err) return res.status(500).json(err);
+  if(data.length ==0) return res.status(404).json("email not found");
+  const hashedPassword = result[0].password
+  
+    const isPasswordCorrect= bcrypt.compare(password, hashedPassword)
+    if(!isPasswordCorrect)
+      return res.status(404).json("invalid email or password")
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+});
+}
+
+
+
+
+export const loogout=(req,res)=>{
+res.clearCookie("access_token",{
+  sameSite:"none",
+  secure:true
+}).status(200).json("user Loged out")   
+};
+
+
