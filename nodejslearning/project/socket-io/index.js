4ed3684600b46d4c18e61d3f196c83c2a27dace0
sundasalmani18 <sundasalmani18@gmail.com>
2024-08-15 20:24:36 +0500

    update

--- nodejslearning/project/socket-io/index.js
+++ nodejslearning/project/socket-io/index.js
@@ -1,21 +1,39 @@
+// const express =require("express") 
+// const http=require("http")
+// const socketIo= require("socket.io")
 import express from "express"
-import http from "http"
+import http from "http" 
 import path from "path"
-import { Server } from "socket.io";
+import {Server} from "socket.io"
 
 
-const app =express()
+
+const app = express()
 const server= http.createServer(app)
 const io = new Server(server)
-io.on("connection", socket => {
-    console.log("user has been connected" ,socket.id)
+
+app.use(express.static("public"))
+io.on("connection", (socket) => {
+    console.log("user connected")
+    socket.on("chat-message",(msg)=>{
+        io.emit("chat-message",msg)
+        console.log(msg)
+    });
+    socket.on("disconnect",(msg)=>{
+
+        console.log("user disconnected")
+    });
+
+
  });
 
-app.use(express.static(path.resolve("./public")))
 
-app.get("/",(req,res)=>{
-res.sendFile("/public/index.html")
-})
+
+// app.use(express.static(path.resolve("./public")))
+
+// app.get("/",(req,res)=>{
+// res.sendFile("/public/index.html")
+// })
 
 
 
