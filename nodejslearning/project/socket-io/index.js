5956286e5444715efee629dcb0e3fe84d159399f
sundasalmani18 <sundasalmani18@gmail.com>
2024-08-16 22:44:38 +0500

    update

--- nodejslearning/project/socket-io/index.js
+++ nodejslearning/project/socket-io/index.js
@@ -6,26 +6,41 @@ import http from "http"
 import {Server} from "socket.io"
 
 
-
-
-const app = express()
-const server= http.createServer(app)
-const io = new Server(server)
-app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
-app.use(express.static("public"))
-io.on("connection", (socket) => {
+const app =express()
+const server =http.createServer(app)
+const io =new Server(server)
+ app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
+ app.use(express.static("public"))
+ io.on("connection",(socket)=>{
     console.log("user connected")
     socket.on("chat-message",(msg)=>{
         io.emit("chat-message",msg)
-        console.log("message",msg)
+        console.log("chat-message",msg)
     });
     socket.on("disconnect",(msg)=>{
-
         console.log("user disconnected")
     });
+ })
 
 
- });
+// const app = express()
+// const server= http.createServer(app)
+// const io = new Server(server)
+// app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
+// app.use(express.static("public"))
+// io.on("connection", (socket) => {
+//     console.log("user connected")
+//     socket.on("chat-message",(msg)=>{
+//         io.emit("chat-message",msg)
+//         console.log("message",msg)
+//     });
+//     socket.on("disconnect",(msg)=>{
+
+//         console.log("user disconnected")
+//     });
+
+
+//  });
 
 
 
