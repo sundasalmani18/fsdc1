c1346f4a2d21e18fce23778de6b5d07f0934c96b
sundasalmani18 <sundasalmani18@gmail.com>
2024-08-24 19:10:29 +0500

    update

---
+++ nodejslearning/project/webrtc/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import http from 'http';
+import {Server} from 'socket.io'
+import path from 'path'
+
+
+
+const app=express()
+const server= http.createServer(app)
+
+const io= new Server(server)
+
+app.use(express.static(path.join(__dirname,"public")))
+
+io.on("connection",(socket)=>{
+    console.log("user connected")
+    socket.on("disconnected",()=>{
+        console.log("user disconnected")
+    })
+    socket.on("offer",(offer)=>{
+        socket.boardcast.emit("offer",offer)
+    })
+    socket.on("answer",(answer)=>{
+        socket.boardcast.emit("answer",answer)
+    });
+    socket.on("ice-candidate",(candidate)=>{
+        socket.boardcast.emit("answer",candidate)
+    });
+   
+})
+
+const PORT =3500;
+server.listen(PORT ,()=>console.log(`server is running on ${PORT}`))
