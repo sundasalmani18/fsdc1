7ff442b5fa6b25252637566173e6784db3707e81
sundas <sundasalmani18@gmail.com>
2024-10-18 22:36:12 +0500

    update

--- reactProjects/personal-portfolio/src/component/skill.jsx
+++ reactProjects/personal-portfolio/src/component/skill.jsx
@@ -2,15 +2,28 @@ import React from 'react'
 
 const Skill = () => {
     return (
-        <section id="skill" class="container my-5">
+        <div id="skill" >
+            <div class="container">
             <h2 class="text-center">Skills</h2>
-            <div class="row">
-                <div class="col-md-6">
-                    <h5>JavaScript</h5>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" style={{width: "80%;"}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
-                    </div>
+            <div class="row justify-content-center pb-5">
+                <div class="col-md-12 heading-section text-center ftco-animate fadeInUp ftco-animated">
+                    <h2 class="mb-4" >My Skills</h2>
                 </div>
+                <div className='row d-flex'>
+                    <div className='col-md-6'>
+                        <div className='progress-wrap ftco-animate fadeInUp ftco-animated'>
+                            <h3>React</h3>
+                            <div class="progress">
+  <div class="progress-bar" role="progressbar" style={{width: "85%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+  <span>85%</span>
+</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
                 <div class="col-md-6">
                     <h5>Python</h5>
                     <div class="progress">
@@ -28,10 +41,17 @@ const Skill = () => {
                     <div class="progress">
                         <div class="progress-bar" role="progressbar" style={{width: "75%;"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
                     </div>
+
+
+
+                    <div class="progress">
+  <div class="progress-bar" role="progressbar" style={{width: "25%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
                 </div>
             </div>
-        </section>
-
+        </div>
+</div>
 
     )
 }
