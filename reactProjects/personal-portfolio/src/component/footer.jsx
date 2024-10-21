819a50581936f6fda1c3dd369dcbd5236fa4e1c5
sundas <sundasalmani18@gmail.com>
2024-10-21 14:29:29 +0500

    update

--- reactProjects/personal-portfolio/src/component/footer.jsx
+++ reactProjects/personal-portfolio/src/component/footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <div id='footer'>
+      <div class="container">
+        <div className='row'>
+          <div className='col-md mb-5'>
+            <h2>Link</h2>
+            <ul>
+              <li>< FontAwesomeIcon icon="fa-solid fa-right-long" />Education</li>
+              <li>Experience</li>
+              <li>Project</li>
+              <li>Skill</li>
+            </ul>
+          </div>
+          <div className='col-md mb-5'>
+            <h2>Services</h2>
+          </div>
+          <div className='col-md mb-5'>
+          < FontAwesomeIcon icon={faUtensils} className=" icon" />
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
   )
 }
 
