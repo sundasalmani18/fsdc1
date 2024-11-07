3e1a4f6f9f2d925504183e06ae759eee3c742b58
sundas <sundasalmani18@gmail.com>
2024-11-07 12:18:43 +0500

    update

--- nodejslearning/project/portofolio-mongo/app.js
+++ nodejslearning/project/portofolio-mongo/app.js
@@ -45,7 +45,7 @@ app.use("/supportsystem", supportRoutes);
 
 
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`"server running on port ${PORT}`)
 })
--- nodejslearning/project/portofolio-mongo/config/db.js
+++ nodejslearning/project/portofolio-mongo/config/db.js
@@ -1,4 +1,7 @@
-const   url= 'mongodb+srv://sundasalmani18:DmRAzYZhaF6CgO9M@clustersundus.ymej4.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSundus';
+
+import 'dotenv/config.js'
+
+const   url=process.env.DATABASE
 export default{ url}
 
 
