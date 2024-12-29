4e37defd1f280ba2f47039546ee4d835160224f3
sundas <sundasalmani18@gmail.com>
2024-12-29 22:47:23 +0500

    update

--- reactProjects/personal-portfolio/src/routes/layoutRoutes.jsx
+++ reactProjects/personal-portfolio/src/routes/layoutRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route ,  Navigate } from "react-router-dom";
 import { useState } from "react";
 import HomePage from "../pages/homePage.jsx"
 import TechnicalsupportPage from "../pages/technicalsupportPage.jsx"
@@ -12,16 +12,22 @@ export default function LayoutRoutes() {
   // const token = localStorage.getItem('token'); // Get the JWT token
   // const userId = localStorage.getItem('userId'); // Get the user ID
   // const username = localStorage.getItem('username');
+
+  const isUserLoggedIn = false;
      return (
       <>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/technicalsupport" element={<TechnicalsupportPage />} />
           <Route path="/admin" element={<AdminchatPage />} />
           <Route path="/user" element={<Userchat />} />
-          <Route path="/register" element={<Register />} />
-
           <Route path="/login" element={<Login setToken={setToken}/>} />
+        
+        {/* If the user is not logged in, redirect to Register */}
+        <Route path="/technicalsupport" element={isUserLoggedIn ? <TechnicalsupportPage /> : <Navigate to="/register" />} />
+
+        <Route path="/register" element={<Register />} />
+        {/* Define other routes here */}
+
 
          
         </Routes>
