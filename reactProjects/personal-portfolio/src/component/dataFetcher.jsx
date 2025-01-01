0e68b283eca56f734ce7b3a825478c9bffd180aa
sundas <sundasalmani18@gmail.com>
2025-01-02 00:53:34 +0500

    update

--- reactProjects/personal-portfolio/src/component/dataFetcher.jsx
+++ reactProjects/personal-portfolio/src/component/dataFetcher.jsx
@@ -10,14 +10,14 @@ const DataFetcher = ({ setData, isLocalData }) => {
     const fetchFromMongoDB = async () => {
         try {
 
-            const response = await axios.get('https://sundusbk.vercel.app/portfolio');
-               console.log(response.data,"response")
-               setData(response.data);
+            // const response = await axios.get('https://sundusbk.vercel.app/portfolio');
+            //    console.log(response.data[0],"response")
+            //    setData(response.data[0]);
 
-            // const response = await fetch('http://localhost:8080/portfolio'); // Replace with your API endpoint
-            // if (!response.ok) throw new Error('Network response was not ok');
-            // const mongoData = await response.json();
-            //setData(mongoData[0]);
+            const response = await fetch('https://sundusbk.vercel.app/portfolio'); // Replace with your API endpoint
+            if (!response.ok) throw new Error('Network response was not ok');
+            const mongoData = await response.json();
+            setData(mongoData[0]);
         } catch (error) {
             console.error('Error fetching data:', error);
             setData(localData); // Fallback to local data
