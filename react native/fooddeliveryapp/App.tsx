b873422e17859943b14a20b790fbdf3aeb5f9ead
sundas <sundasalmani18@gmail.com>
2026-06-02 12:47:23 +0500

    update

---
+++ react native/fooddeliveryapp/App.tsx
@@ -0,0 +1,18 @@
+import {Text,View} from "react-native";
+// import { StatusBar } from 'expo-status-bar';
+
+// import './global.css';
+
+export default function App() {
+  return (
+   <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Hello to my app</Text>
+    </View>
+  );
+}
